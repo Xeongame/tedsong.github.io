698d19eed508b5e25bff64cf6d4cd9f7383b9dd4
Xeongame <64991759+Xeongame@users.noreply.github.com>
2024-09-11 12:16:09 -0600

    none

---
+++ 01 Drawing/sketch.js
@@ -0,0 +1,21 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  fill(255, 0, 0)
+  circle(width, height, 50)
+  circle(0, height, 50)
+  circle(0, 0, 50)
+  circle(width, 0, 50)
+  circle(width*2/3, height/2, 50)
+}
