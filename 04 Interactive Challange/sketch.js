d623cc7412001cfdc1b2fd1a883dce2c333e6d77
Xeongame <64991759+Xeongame@users.noreply.github.com>
2024-09-18 12:16:35 -0600

    added hills and keyboard control

--- 04 Interactive Challange/sketch.js
+++ 04 Interactive Challange/sketch.js
@@ -7,7 +7,9 @@
 
 let currentBack = 0;
 let sizeX = 800, sizeY = 500;
-let horizon = 400;
+let sunSize = sizeX/8;
+let sunSizeMultiplier = 1;
+let horizon = 480;
 let sceneConfig = [ // assign different scene settings through dictionary
   {
     id:0,
@@ -15,10 +17,31 @@ let sceneConfig = [ // assign different scene settings through dictionary
     terrainColor1:[225],
     terrainColor2:[235],
     terrainColor3:[248],
+    hillColor:[250], 
   },
   {
     id:1,
-    color:"Red",
+    backColor:[150, 170, 255],
+    terrainColor1:[225],
+    terrainColor2:[235],
+    terrainColor3:[248],
+    hillColor:[250], 
+  },
+  {
+    id:2,
+    backColor:[150, 170, 255],
+    terrainColor1:[225],
+    terrainColor2:[235],
+    terrainColor3:[248],
+    hillColor:[250], 
+  },
+  {
+    id:3,
+    backColor:[150, 170, 255],
+    terrainColor1:[225],
+    terrainColor2:[235],
+    terrainColor3:[248],
+    hillColor:[250], 
   },
 ];
 
@@ -29,32 +52,67 @@ function setup() {
 
 function draw() {
   let config = sceneConfig[currentBack]; //gets settings
+  let backColor = config.backColor
   background(220);
   
   fill(config.backColor); //background
   rect(0, 0, sizeX, sizeY); 
 
-  
-
-  fill(230, 227, 200) //moon
-  stroke(222, 215, 196)
-  circle(mouseX, sizeY - mouseY + horizon/2, sizeX/8) //mirror y pos from the horizon
+  fill(250, 247, 140); //moon
+  stroke(255, 255, 200);
+  strokeWeight(2)
+  circle(mouseX, sizeY - mouseY + horizon/2, sunSize * sunSizeMultiplier); //mirrors y pos from the horizon and scaling with size variable
   fill(config.backColor);
-  noStroke()
-  circle(mouseX - sizeX/26, sizeY - mouseY - sizeX/26 + horizon/2, sizeX/8)
+  noStroke();
+  circle(mouseX - (sunSize/5 * sunSizeMultiplier), sizeY - mouseY - (sunSize/5 * sunSizeMultiplier) + horizon/2, sunSize * sunSizeMultiplier); //creates a new circle to blend with background to give the moon the crescent shape
 
   strokeWeight(2); //sun at the mouse pos
-  fill(255, 239, 0)
+  fill(255, 239, 0);
   stroke(255, 220, 0);
-  circle(mouseX, mouseY, sizeX/8)
+  circle(mouseX, mouseY, sunSize * sunSizeMultiplier); //scales with size variable
 
-  fill(config.terrainColor1) //terrain
+  fill(config.hillColor - 50); //hills
   noStroke();
-  ellipse(sizeX - 250, sizeY + 100, 1500, 600)
-  fill(config.terrainColor2)
-  ellipse(sizeX - 500, sizeY + 80, 1000, 600)
-  fill(config.terrainColor3)
-  ellipse(sizeX - 200, sizeY + 180, 700, 600)
+  triangle(410, 310, 531, 118, 658, 311);
+  triangle(690, 320, 740, 235, 785, 321);
+  fill(config.hillColor);
+  triangle(690, 320, 740, 235, 755, 320);
+  triangle(400, 310, 531, 118, 608, 311);
 
-  print(mouseY)
+  fill(config.terrainColor1); //terrain
+  noStroke();
+  ellipse(sizeX - 250, sizeY + 100, 1500, 600);
+  fill(config.terrainColor2);
+  ellipse(sizeX - 550, sizeY + 80, 1100, 600);
+  fill(config.terrainColor3);
+  ellipse(sizeX - 200, sizeY + 180, 700, 600);
+
+  //print(mouseX, mouseY)
+};
+
+function keyPressed(){
+  if (keyCode == RIGHT_ARROW){ //changes sun/moon size with arrow keys
+    sunSizeMultiplier += 0.1;
+  } else if (keyCode == LEFT_ARROW){
+    sunSizeMultiplier -= 0.1;
+  };
+
+  if (sunSizeMultiplier > 1.5){ //resets size if size is too big or too small
+    sunSizeMultiplier = 0.5;
+  } else if (sunSizeMultiplier < 0.5){
+    sunSizeMultiplier = 1.5;
+  };
+
+};
+
+function mouseClicked(){
+  if (mouseButton === CENTER || mouseButton === LEFT){
+    currentBack += 1
+
+    if (currentBack === 4) {
+      currentBack = 0
+    }
+  };
+
+  print(mouseButton, currentBack)
 };
