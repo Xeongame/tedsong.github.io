3ff26cf114292384a0d1b98e6f841b6a06f512a2
Xeongame <64991759+Xeongame@users.noreply.github.com>
2024-11-18 12:19:49 -0600

    added comments

--- 15 Balloon Tree/sketch.js
+++ 15 Balloon Tree/sketch.js
@@ -1,34 +1,34 @@
-// Project Title
-// Your Name
-// Date
+// Balloon Tree
+// Ted Song
+// 11/18/2024
 //
 // Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// - all mentionned on the assignment page
 
 
-let scale = 15;
+let scale = 15; //scale of branches 
 let leafSize = 5;
-let step = 0;
-let leafDepth = 0;
-let stepMultiplier;
-let angleSpread = 15;
+let step = 0; //count for each iteration of leaf generation
+let leafDepth = 1; //modifier for showing leaves
+let stepMultiplier; //used for random color
+let angleSpread = 15; 
 
 function setup() {
   createCanvas(500, 500);
   background(255);
   
-  stepMultiplier = random(50, 200);
+  stepMultiplier = random(50, 200); //sets a random multiplier at the start
 }
 
 function draw() {
   background(255);
-  step = 0;
+  step = 0; //aka depth, counts up 1 every iteration but resets at the start every frame
   drawTree(width/2, height*.9, 90, 6);
 }
 
 function drawLine( x1,  y1,  x2,  y2,  depth) {
   //draw a line segment connecting (x1,y1) to (x2,y2)
-  fill(0)
+  fill(0);
   strokeWeight(sqrt(depth) * depth/2)
   line(x1, y1, x2, y2);
   drawLeaf(x2, y2, depth);
@@ -36,23 +36,24 @@ function drawLine( x1,  y1,  x2,  y2,  depth) {
 }
 
 function drawLeaf(x, y, depth) {
-  if (depth < leafDepth) {
-    strokeWeight(1)
-    randomSeed(step * stepMultiplier)
-    fill(random(0, 255), random(0, 255), random(0, 255));
+  if (depth < leafDepth) { //draw leaf only if the depth less than the max depth set by the user
+    strokeWeight(1);
+    randomSeed(step * stepMultiplier); //random but CONSISTENT color generation based on count variable
+    fill(random(0, 255), random(0, 255), random(0, 255), 127);
     circle(x, y, leafSize + scale * sqrt(depth + 0.25) * random(0.5, 1.2));
 
-    step += 1;
+    step += 1; //count step up
   }
 }
 
-function drawTree(x1, y1, angle, depth) {
+function drawTree(x1, y1, angle, depth) { //preset 
   if (depth > 0) {
     let x2 = x1 + (cos(radians(angle))*depth*scale);     //calculate endpoints of current branch
     let y2 = y1 - (sin(radians(angle))*depth*scale);     //using trig ratios. Get shorter based on depth
 
     drawLine(x1, y1, x2, y2, depth);
-    let spread = angleSpread + mouseX/width * angleSpread * 0.7;
+    let spread = angleSpread + mouseX/width * angleSpread * 0.7; //determines spread based on how far the mouse is from the right side of the screen
+
     //for a 3-branch tree:
     drawTree(x2, y2, angle-spread, depth-1);
     drawTree(x2, y2, angle+spread, depth-1);
@@ -61,9 +62,15 @@ function drawTree(x1, y1, angle, depth) {
 }
 
 function keyPressed() {
-  if (key === "z") {
+  if (key === "z") { //z decreases max depth
+    leafDepth -= 1;
+  } else if (key === "x") { //x increases max depth
+    leafDepth += 1;
+  }
 
-  } else if (key === "x") {
-    
+  if (leafDepth > 7) { //caps at max 7 depth and min 1 depth
+    leafDepth = 7;
+  } else if (leafDepth < 1) {
+    leafDepth = 1;
   }
 }
