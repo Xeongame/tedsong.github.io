0fc5cc99792e523f05724fc1f1eb5fd9bbfaa96d
Xeongame <64991759+Xeongame@users.noreply.github.com>
2024-12-05 11:26:23 -0600

    pacman continuation

--- Pac-Man/sketch.js
+++ Pac-Man/sketch.js
@@ -13,8 +13,9 @@ let length = 30;
 
 let pacX = 0;
 let pacY = 0;
-let pacSpeed = 5;
+let pacSpeed = 7.4;
 let grids = [];
+let paths = [];
 let player 
 
 class pac {
@@ -36,49 +37,56 @@ class pac {
   }
 
   move() {
-    let nextGrid 
+    let grid = grids[this.row][this.column]
+    let path = paths[this.row][this.column]
+    let choosenPath
+    let index
+
+    function find(n) {
+      return n === 
+    }
 
     if (this.dir === "right") { 
-      nextGrid = grids[this.row][this.column + 1];
-      this.moveX = pacSpeed;
+      choosenPath = path[0]
+      index = choosenPath.findIndex(find);
 
     } else if (this.dir === "left") { 
       nextGrid = grids[this.row][this.column - 1];
       this.moveX = -pacSpeed;
 
     } else if (this.dir === "up") { 
-      grids[this.row - 1];
-      this.moveY = -pacSpeed;
-
+      if (grids[this.row - 1]) {
+        nextGrid = grids[this.row - 1][this.column];
+        this.moveY = -pacSpeed;
+        print(nextGrid, this.row)
+      }
     } else { 
-      nextGrid = grids[this.row + 1]; 
-      this.moveY = pacSpeed
+      if (grids[this.row + 1]) {
+        nextGrid = grids[this.row + 1][this.column]; 
+        this.moveY = pacSpeed
+      }
     }
 
-    print(nextGrid)
+    //print(nextGrid)
     if (nextGrid) {
-      this.x += this.moveX;
-      this.y += this.moveY;
-
       let dirX = this.moveX/Math.abs(this.moveX)
       let dirY = this.moveY/Math.abs(this.moveY)
-      let centerX = nextGrid[0] + length/2
-      let centerY = nextGrid[1] + length/2
 
-      if (this.x * dirX > centerX * dirX) {
+      if (this.x * dirX > nextGrid[0] * dirX) {
         this.column += dirX;
       }
-
-      if (this.y * dirY > centerY * dirY) {
+      print(this.row, dirY, this.moveY, nextGrid[1])
+      if (this.y * dirY > nextGrid[1] * dirY) {
         this.row += dirY;
+        
       }
+
+      this.x += this.moveX;
+      this.y += this.moveY;
     } else {
       this.x = grids[this.row][this.column][0] + length/2
       this.y = grids[this.row][this.column][1] + length/2;
     }
-    
-    print(nextGrid)
-
   }
 }
 
@@ -99,7 +107,48 @@ function setup() {
     }
   }
 
-  print(grids)
+  for (let x = 0; x < columnNum; x++) {
+    for (let y = 0; y < rowNum; y++) {
+      let row = paths[y] || [];
+      let xPoints = []
+      let yPoints = []
+      let centerX = x * length + length/2
+      let centerY = y * length + length/2
+
+      if (x === 0) {
+        for (let i = centerX; i < centerX + length/2; i++) {
+          xPoints.push([i, centerY])
+        }
+      } else if (x === columnNum - 1) {
+        for (let i = centerX; i >= centerX - length/2; i--) {
+          xPoints.push([i, centerY])
+        }
+      } else {
+        for (let i = centerX - length/2; i < centerX + length/2; i++) {
+          xPoints.push([i, centerY])
+        }
+      }
+
+      if (y === 0) {
+        for (let i = centerY; i < centerY + length/2; i++) {
+          yPoints.push([centerX, i])
+        }
+      } else if (y === rowNum - 1) {
+        for (let i = centerY; i >= centerY - length/2; i--) {
+          yPoints.push([centerX, i])
+        }
+      } else {
+        for (let i = centerY - length/2; i < centerY + length/2; i++) {
+          yPoints.push([centerX, i])
+        }
+      }
+
+      row[x] = [xPoints, yPoints]
+      paths[y] = row
+    }
+  }
+
+  print(grids, paths)
   player = new pac();
 }
 
@@ -133,4 +182,16 @@ function draw() {
   drawMap()
   player.show()
   player.move()
+}
+
+function keyPressed() {
+  if (key === "w") {
+    player.dir = "up"
+  } else if (key === "s") {
+    player.dir = "down"
+  } else if (key === "a") {
+    player.dir = "left"
+  } else if (key === "d") {
+    player.dir = "right"
+  }
 }
