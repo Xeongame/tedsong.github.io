a4c04e905f678aaa4c8ca41c01a46764b7bfc2f5
Xeongame <64991759+Xeongame@users.noreply.github.com>
2025-01-22 19:46:22 -0600

    Snake
    - Started snake game

---
+++ f/sketch.js
@@ -0,0 +1,15 @@
+// First Day Demo
+// Ted Song
+// 9/10/2024
+//
+// Extra for Experts:
+// draw happens at 60fps in loop
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
